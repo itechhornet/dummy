13f7624a7ba1e9282e16e4dc723222605dac0957
itechhornet <65245179+itechhornet@users.noreply.github.com>
2020-06-13 19:39:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
-const mysql = require('mysql');
 const express = require('express');
-const bodyparser = require('body-parser');
 var app = express();
-//Configuring express server
-app.use(bodyparser.json());
-
-var mysqlConnection = mysql.createConnection({
-    host: '162.241.225.135',
-    user: 'itechhor_admin',
-    password: 'nOmrZ2kY[m2N',
-    database: 'itechhor_revive_test',
-    port : 3306,
-    multipleStatements: true
-    });
-
-    mysqlConnection.connect((err)=> {
-        if(!err)
-        console.log('Connection Established Successfully');
-        else
-        console.log('Connection Failed!'+ JSON.stringify(err,undefined,2));
-        });
-        //Establish the server connection
-//PORT ENVIRONMENT VARIABLE
 const port = process.env.PORT || 8080;
+app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Listening on port ${port}..`));   
